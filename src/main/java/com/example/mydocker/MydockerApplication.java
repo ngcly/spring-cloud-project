5da87018509cf4b5a240a4c639d4983b7aaca857
chenning <531237716@qq.com>
2019-05-09 17:24:20 +0800

    Initial commit

---
+++ src/main/java/com/example/mydocker/MydockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydockerApplication.class, args);
+    }
+
+}
