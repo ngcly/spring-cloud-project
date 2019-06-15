5c78293cbb8e8f5628c9ac3e3b363d30ede5205d
chenning <531237716@qq.com>
2019-06-15 17:56:32 +0800

    新增代码

--- spring-cloud-eureka/src/main/java/com/cn/eureka/config/WebSecurityConfig.java
+++ spring-cloud-eureka/src/main/java/com/cn/eureka/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cn.eureka.config;
 
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,7 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().ignoringAntMatchers("/eureka/**");
         super.configure(http);
     }
 }
