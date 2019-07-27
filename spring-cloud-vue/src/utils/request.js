147699e41781de31e310ac186499b58046545b6a
chenning <531237716@qq.com>
2019-07-27 17:57:28 +0800

    xiugai vue

--- spring-cloud-vue/src/utils/request.js
+++ spring-cloud-vue/src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: 'http://localhost:8002/', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
