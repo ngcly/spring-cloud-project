ce9f4f399ab79c6c6d0a5df4d24df360f5602668
chenning <531237716@qq.com>
2019-08-03 17:55:29 +0800

    添加其他服务

---
+++ spring-cloud-other/src/main/java/com/cn/other/config/ResourceServerConfig.java
@@ -0,0 +1,32 @@
+package com.cn.other.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author chenning
+ * @Classname ResourceServerConfig
+ * @Description 资源配置
+ * @Date 2019/6/25 16:42
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**","/druid/**","/v2/api-docs/**").permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
